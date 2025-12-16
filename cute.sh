dfd65ac865f9f431e46987d1b4b3e77087355a21
ras0q <ras0q@users.noreply.github.com>
2025-12-17 01:45:27 +0900

    refactor: split functions

--- cute.sh
+++ cute.sh
@@ -1,7 +1,15 @@
 #!/bin/sh -eu
 
-cute() {
-  local cute_usage='Cute: A CLI tool to exe"CUTE"s commands from markdown files.
+local cute_color_success="\033[32m"
+local cute_color_error="\033[31m"
+local cute_color_prompt="\033[90m"
+
+local cute_debug_mode=0
+
+
+_cute_usage() {
+  cat <<EOF
+Cute: A CLI tool to exe"CUTE"s commands from markdown files.
 
 Usage:
   cute [-h] [-d] [TASK_NAME|SLUG ...]
@@ -15,28 +23,16 @@ Arguments:
                   Multiple tasks can be specified to execute them in order.
 
 Example:
-  cute                    # Interactive mode with fzf
-  cute -d                 # Enable debug mode with fzf selection
-  cute build              # Execute task with slug "build"
-  cute "Build Project"    # Execute task by name
-  cute build test deploy  # Execute multiple tasks in order'
+  cute                   # Interactive mode with fzf
+  cute -d                # Enable debug mode with fzf selection
+  cute build             # Execute task with slug "build"
+  cute "Build Project"   # Execute task by name
+  cute build test deploy  # Execute multiple tasks in order
+EOF
+}
 
-  local cute_color_success="\033[32m"
-  local cute_color_error="\033[31m"
-  local cute_color_prompt="\033[90m"
-
-  local cute_debug_mode=0
-
-  while getopts "hd" opt; do
-    case "$opt" in
-      h) echo "$cute_usage"; return 0 ;;
-      d) cute_debug_mode=1 ;;
-    esac
-  done
-  shift $((OPTIND - 1))
-
-  local cute_files="$(find . -type f -name "*.md" -o -name "*.markdown" | sort)"
-  local cute_tasks=$(echo "$cute_files" | xargs awk -v sep="\x1f" '
+_cute_extract_tasks() {
+  echo "$@" | xargs awk -v sep="\x1f" '
     function slugify(str) {
       gsub(/[^a-zA-Z0-9]+/, "-", str)
       str = tolower(str)
@@ -83,10 +79,50 @@ Example:
       task_name = m[1];
       next
     }
-  ')
+  '
+}
 
-  local cute_task_names=$(echo "$cute_tasks" | awk -F'\x1f' '{print $1 " (" $2 ")"} ')
-  local cute_duplicate_slugs=$(echo "$cute_tasks" | awk -F'\x1f' '{print $2}' | uniq -d)
+_cute_execute_task() {
+  local cute_task="$@"
+  local cute_task_name=$(echo "$cute_task" | awk -F'\x1f' '{print $1}')
+  local cute_shell=$(echo "$cute_task" | awk -F'\x1f' '{print $3}')
+  local cute_command=$(echo "$cute_task" | cut -d$'\x1f' -f4- | sed "s/$(print '\x1f')/\n/g")
+  if [ -z "$cute_command" ]; then
+    echo "No command found for task '$cute_task_name'."
+    return 1
+  fi
+
+  (
+    if [ $cute_debug_mode -eq 1 ]; then
+      printf "${cute_color_success}▶ Executing task: %s\033[0m\n" "$cute_task_name"
+    fi
+
+    export PS4="$(printf "${cute_color_prompt}[%s]$ \033[0m" "$cute_task_name")"
+    script -eq -c "$cute_shell -c 'set -ux; $cute_command'" /dev/null
+    local exit_code=$?
+    if [ $exit_code -ne 0 ]; then
+      return $exit_code
+    fi
+
+    if [ $cute_debug_mode -eq 1 ]; then
+      printf "${cute_color_success}✔ Completed task: %s\033[0m\n" "$cute_task_name"
+    fi
+  )
+}
+
+cute() {
+  while getopts "hd" opt; do
+    case "$opt" in
+      h) _cute_usage; return 0 ;;
+      d) cute_debug_mode=1 ;;
+    esac
+  done
+  shift $((OPTIND - 1))
+
+  local cute_files="$(find . -type f -name "*.md" -o -name "*.markdown" | sort)"
+  local cute_tasks=$(_cute_extract_tasks "$cute_files")
+
+  local cute_duplicate_slugs=$(echo "$cute_tasks" | awk -F'\x1f' '{print $2}' | sort | uniq -d)
   if [ -n "$cute_duplicate_slugs" ]; then
     echo "Duplicate task found: $cute_duplicate_slugs"
     return 1
@@ -94,7 +130,8 @@ Example:
 
   local cute_requested_tasks="$@"
   if [ -z "$cute_requested_tasks" ]; then
-    local cute_task_name=$(echo "$cute_task_names" | fzf --prompt="Select a task to execute: ")
+    local cute_task_names=$(echo "$cute_tasks" | awk -F'\x1f' '{print $1 "\t" $2} ')
+    local cute_task_name=$(echo "$cute_task_names" | fzf --prompt="Select a task to execute: " | awk -F'\t' '{print $2}')
     if [ -z "$cute_task_name" ]; then
       echo "No task selected."
       return 1
@@ -103,40 +140,13 @@ Example:
   fi
 
   for cute_task_identifier in $cute_requested_tasks; do
-    if echo "$cute_task_identifier" | grep -q " ("; then
-      local cute_task=$(echo "$cute_tasks" | awk -F'\x1f' -v task="$cute_task_identifier" '$1 " (" $2 ")" == task {print $0; exit}')
-    else
-      local cute_task=$(echo "$cute_tasks" | awk -F'\x1f' -v identifier="$cute_task_identifier" '$1 == identifier || $2 == identifier {print $0; exit}')
-    fi
+    local cute_task=$(echo "$cute_tasks" | awk -F'\x1f' -v identifier="$cute_task_identifier" '$1 == identifier || $2 == identifier {print $0; exit}')
 
     if [ -z "$cute_task" ]; then
       echo "Task not found: '$cute_task_identifier'"
       return 1
     fi
 
-    local cute_task_name=$(echo "$cute_task" | awk -F'\x1f' '{print $1}')
-    local cute_shell=$(echo "$cute_task" | awk -F'\x1f' '{print $3}')
-    local cute_command=$(echo "$cute_task" | cut -d$'\x1f' -f4- | sed "s/$(print '\x1f')/\n/g")
-    if [ -z "$cute_command" ]; then
-      echo "No command found for task '$cute_task_name'."
-      return 1
-    fi
-
-    (
-      if [ $cute_debug_mode -eq 1 ]; then
-        printf "${cute_color_success}▶ Executing task: %s\033[0m\n" "$cute_task_name"
-      fi
-
-      export PS4="$(printf "${cute_color_prompt}[%s]$ \033[0m" "$cute_task_name")"
-      script -eq -c "$cute_shell -c 'set -ux; $cute_command'" /dev/null
-      local exit_code=$?
-      if [ $exit_code -ne 0 ]; then
-        return $exit_code
-      fi
-
-      if [ $cute_debug_mode -eq 1 ]; then
-        printf "${cute_color_success}✔ Completed task: %s\033[0m\n" "$cute_task_name"
-      fi
-    )
+    _cute_execute_task "$cute_task"
   done
 }
