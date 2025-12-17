ccd12a9ae5cd6e8beb606ba9cd00bef9e2325429
ras0q <ras0q@users.noreply.github.com>
2025-12-17 14:41:58 +0900

    feat: add completions

--- functions/cute.fish
+++ functions/cute.fish
@@ -17,3 +17,13 @@ function cute --description "Execute commands from markdown files"
 
     sh -c ". $cute_script && cute \$@" -- $argv
 end
+
+function __fish_cute_tasks --description 'Get available cute tasks'
+    cute -l 2>/dev/null || true
+end
+
+complete -c cute -f
+complete -c cute -n "__fish_seen_subcommand_from" -s h -d "Show help message"
+complete -c cute -n "__fish_seen_subcommand_from" -s l -d "List tasks"
+complete -c cute -n "__fish_seen_subcommand_from" -s v -d "Enable verbose mode"
+complete -c cute -a "(__fish_cute_tasks)" -d "Task slug"
